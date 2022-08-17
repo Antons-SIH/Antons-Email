335d01a785aa7fbcce151e49e4d1ee8617bbdc00
Atharva <atharvapradeeprao.k20@iiits.in>
2022-08-17 18:00:26 +0530

    Profile: added

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { verified, reupload } = require("../controllers/user.controller");
 const userRouter = express.Router();
-
+//routes to send email
 userRouter.post("/verified", verified);
 userRouter.post("/reupload", reupload);
 module.exports = userRouter;
