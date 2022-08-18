2a949133ad5b241e8419fb04a19aed0d57b56c28
Atharva <atharvapradeeprao.k20@iiits.in>
2022-08-18 17:15:46 +0530

    sendmail :added

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { verified, reupload, sendMail } = require("../controllers/user.controller");
 const userRouter = express.Router();
-//routes to send email
+//routes to send mail
 userRouter.post("/verified", verified);
 userRouter.post("/reupload", reupload);
 userRouter.post("/sendmail",sendMail)
